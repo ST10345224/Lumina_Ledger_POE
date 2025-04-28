288fb32b98f7415e0a7099475dac2cb0fcfd0605
ST10345224 <keval.balgobind@gmail.com>
2025-04-28 20:47:29 +0200

    Created Register and Login Activities. Created User, Expense, Category data classes

---
+++ app/src/main/java/com/st10345224/luminaledgerpoe/Expense.kt
@@ -0,0 +1,14 @@
+package com.st10345224.luminaledgerpoe
+
+data class Expense(
+    val exID: String,
+    val UserID: String,
+    val Category: String,
+    val exAmount: Double,
+    val Date: String,
+    val exDescription: String,
+    val exPhotoString: String,
+    val Currency: String,
+    val exTitle: String
+
+)
